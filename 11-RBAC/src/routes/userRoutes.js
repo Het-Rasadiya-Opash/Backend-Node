b7d25924487934c8ff4c33267a6f27eb7723c6df
Het <hra.opash@gmail.com>
2026-02-05 18:03:41 +0530

    .

--- 11-RBAC/src/routes/userRoutes.js
+++ 11-RBAC/src/routes/userRoutes.js
@@ -3,12 +3,10 @@ const verfiyToken = require("../middlewares/authMiddleware");
 const authorizeRole = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
-//admin access router
 router.get("/admin", verfiyToken, authorizeRole("admin"), (req, res) => {
   res.json({ message: "Welcome Admin" });
 });
 
-//admin & manager
 router.get(
   "/manager",
   verfiyToken,
@@ -18,7 +16,6 @@ router.get(
   },
 );
 
-// all can access
 router.get(
   "/user",
   verfiyToken,
