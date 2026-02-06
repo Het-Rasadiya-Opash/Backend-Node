b163bf44d44b63480c2c2d213b1e9e809068f50b
Het <hra.opash@gmail.com>
2026-02-06 18:15:10 +0530

    change api

--- 14-Search-Sort-Pagination-API/controllers/product.js
+++ 14-Search-Sort-Pagination-API/controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  const { company, name, featured } = req.query;
+  const { company, name, featured, sort, select } = req.query;
 
   const queryObject = {};
 
@@ -9,21 +9,29 @@ const getAllProducts = async (req, res) => {
   if (featured) queryObject.featured = featured;
   if (name) queryObject.name = { $regex: name, $options: "i" };
 
-  console.log(queryObject)
-
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 15;
   const skip = (page - 1) * limit;
 
-  const products = await Product.find(queryObject)
-    .sort(req.query.sort && req.query.sort.split(",").join(" "))
-    .select(req.query.select && req.query.select.split(",").join(" "))
-    .skip(skip)
-    .limit(limit);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    result = result.sort(sort.split(",").join(" "));
+  }
+
+  if (select) {
+    result = result.select(select.split(",").join(" "));
+  }
+
+  //count number of results fetched
+  const numOfProducts = await Product.countDocuments(queryObject);
+
+  //pagination
+  const products = await result.skip(skip).limit(limit);
 
   res.status(200).json({
     products,
-    nbHits: products.length,
+    numOfProducts,
     page,
   });
 };
